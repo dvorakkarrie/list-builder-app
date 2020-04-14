3dfd0814c0ccf494ab59e88bc1514fd47475740d
Karrie Dvorak <dvorak.karrie@gmail.com>
2020-04-13 22:12:24 -0500

    created components and header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className = "header">
+            <h1>List Builder</h1>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/TodoItem.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TodoItem = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default TodoItem
+
+
+
