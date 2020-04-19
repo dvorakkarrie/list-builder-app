a305721a5c057a06f2c72bd49b402b97824cac29
Karrie Dvorak <dvorak.karrie@gmail.com>
2020-04-19 14:28:06 -0500

    began work on ListItem update

--- src/App.js
+++ src/App.js
@@ -39,6 +39,7 @@ class App extends Component {
       itemDescription: "",
       itemImageUrl: "",
       isAuthenticated: false,
+      isComplete: "",
     };
   }
 
--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -1,25 +1,36 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const ListItems = (props) => {
-    console.log(props)
+  console.log(props);
 
-    return (
-        <div>
-            <Link to={`/items/${props.item._id}`} 
-                key={props.item_id}>
-                <ul>
-                    <li>
-                        {props.item.item_desc}
-                        <button 
-                            className='delete-button'
-                            id={props.item._id}
-                            onClick={props.handleListItemDelete}> - </button>
-                    </li>
-                </ul>
-            </Link>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Link to={`/items/${props.item._id}`} key={props.item_id}>
+        <ul>
+          <li>
+            {props.item.item}
+            <button
+              className="btn-task-list"
+              id={props.item._id}
+              onClick={props.handleListItemDelete}
+            >
+              {" "}
+              <i className="fas fa-check-circle"></i>{" "}
+            </button>
+            <button
+              className="btn-task-list"
+              id={props.item._id}
+              onClick={props.handleListItemDelete}
+            >
+              {" "}
+              <i className="fas fa-trash"></i>{" "}
+            </button>
+          </li>
+        </ul>
+      </Link>
+    </div>
+  );
+};
 
-export default ListItems
+export default ListItems;
